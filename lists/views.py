9f76db33eb6035c489aa85d809650a0677473b99
Elves M. Rodrigues <elvesmateusrodrigues@outlook.com>
2021-08-03 17:12:16 -0300

    Revert "Implementando o Novo Projeto (Listas e Items) - PARTE 2"
    This reverts commit 0b2be229a5700a1da1e41625e0669d4d7560f2fe.

--- lists/views.py
+++ lists/views.py
@@ -9,7 +9,3 @@ def home_page(request):
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
-
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
